f2ad7937f9abb125228fdc269ab6d946fe374f16
yashika569384 <kapooryashika104@gmail.com>
2025-12-17 12:17:47 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 import streamlit as st
-#import artff
-#import newsai
+import artff
+import newsai
 import game
 import home1
 # st.set_page_config(page_title="VibeVerse", layout="centered")
@@ -169,6 +169,7 @@ elif st.session_state.game_button_clicked:
 
 #     if st.button("Back", key="back_news"):
 #         st.session_state.news_button_clicked = False
+
 
 
 
